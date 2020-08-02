d1e14d4c18e33fe5f21ded0df842145844cb68f3
kostya <matvienkoigor956@gmail.com>
2020-08-02 18:52:04 +0300

    adaptive for ipad added slick slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+$(document).ready(function() {
+
+    //--------------------------------------------------------------------- smooth scrolling
+    $(".item_link").on("click", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 500);
+    });
+    // --------------------------------------------------------------------------------------- swiper
+    $slick_slider = $('.swiper');
+    settings_slider = {
+        centerMode: true,
+        dots: true,
+        variableWidth: true,
+        infinite: false,
+        centerPadding: '70px',
+        slidesToShow: 1,
+        initialSlide: 1,
+    }
+
+    slick_on_mobile($slick_slider, settings_slider);
+})
+
+// ajax
 function sendAjaxForm(ajax_form, url) {
     var ajax_form = $('#' + ajax_form);
 
@@ -17,8 +42,6 @@ function sendAjaxForm(ajax_form, url) {
                 opacity: 1
             }, 500);
         }, 600);
-
-
 
         // $.ajax({
         //     url: url,
@@ -42,13 +65,18 @@ function sendAjaxForm(ajax_form, url) {
         return false;
     })
 }
-// ----------------------------------------------------------------------------------------------
 
-$(document).ready(function() {
-    $(".item_link").on("click", function(event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 500);
+// slick on mobile
+function slick_on_mobile(slider, settings) {
+    $(window).on('load resize', function() {
+        if ($(window).width() > 992) {
+            if (slider.hasClass('slick-initialized')) {
+                slider.slick('unslick');
+            }
+            return
+        }
+        if (!slider.hasClass('slick-initialized')) {
+            return slider.slick(settings);
+        }
     });
-});
+};
