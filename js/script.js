eacb2aaa956131bdc9ac3e2041d83a88dc2403c9
kostya <matvienkoigor956@gmail.com>
2020-08-04 11:43:41 +0300

    added content

--- js/script.js
+++ js/script.js
@@ -34,37 +34,32 @@ function sendAjaxForm(ajax_form, url) {
         //     opacity: 0
         // }, 500);
 
-        $('.after_send_hide').animate({
-            bottom: 400,
-            opacity: 0
-        }, 500).hide(500);
 
-        setTimeout(function() {
-            $('.response').addClass('show').animate({
-                top: '25%',
-                opacity: 1
-            }, 500);
-        }, 600);
 
-        // $.ajax({
-        //     url: url,
-        //     type: "POST",
-        //     dataType: "html",
-        //     data: ajax_form.serialize(),
-        //     success: function(response) {
-        //         result = $.parseJSON(response);
+        $.ajax({
+            url: url,
+            type: "POST",
+            dataType: "html",
+            data: ajax_form.serialize(),
+            success: function(response) {
+                result = $.parseJSON(response);
 
-        //         // ajax_form.parent().hide();
-        //         // form_title.hide();
-        //         // $('#anketa').addClass('styles_after_send');
-        //         // result_form.show().animate({
-        //         // opacity: 1
-        //         // }, 500);
-        //     },
-        //     error: function(response) {
-        //         result_form.html(response);
-        //     }
-        // });
+                $('.after_send_hide').animate({
+                    bottom: 400,
+                    opacity: 0
+                }, 500).hide(500);
+
+                setTimeout(function() {
+                    $('.response').addClass('show').animate({
+                        top: '25%',
+                        opacity: 1
+                    }, 500);
+                }, 600);
+            },
+            error: function(response) {
+                result_form.html(response);
+            }
+        });
         return false;
     })
 }
